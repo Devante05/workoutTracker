bb59d6e660760f15dfdf8f35a69dab298e92e53d
Devante W <williams.devante05@gmail.com>
2021-02-27 14:39:07 -0600

    fix mongodb connection

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
 });
 
 // routes
